27cf9fb5c36ffb40d5bd56f87f5a9fe45322d0ff
vanessadean <vanessa.dean@gmail.com>
2014-11-13 16:05:14 -0500

    switch to Sinatra base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require_relative "../../config/environment"
 
-class ApplicationController < Sinatra::Application
+class ApplicationController < Sinatra::Base
 
   get '/tweets' do
     @tweets = Tweet.all
@@ -11,5 +11,14 @@ class ApplicationController < Sinatra::Application
     # Tweet.new(params[:user], params[:status])
     Tweet.create(:user => params[:user], :status => params[:status])
     redirect '/tweets'
+  end
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  configure :development do
+    set :database, "sqlite3:///db/database.db"
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,5 @@
-require 'sinatra'
-require 'pry'
 require 'bundler'
 Bundler.require
 
 require './app/models/tweet'
 
-configure do
-  set :public_folder, 'public'
-  set :views, 'app/views'
-end
-
-configure :development do
-  set :database, "sqlite3:///db/database.db"
-end

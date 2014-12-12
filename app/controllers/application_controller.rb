2774a399f300dc44f0c8fc0621f3e15cc782ac80
vanessadean <vanessa.dean@gmail.com>
2014-12-12 13:04:29 -0500

    Make hash explicit in Tweet.new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
 
   post '/tweets' do
     # Tweet.new(params[:user], params[:status])
-    tweet = Tweet.new(:user => params[:user], :status => params[:status])
+    tweet = Tweet.new({:user => params[:user], :status => params[:status]})
     tweet.save
     redirect '/tweets'
   end

a0b114898b873766fa5faa9fa5dfb899e3552b84
Victoria Friedman <victoria.m.friedman@gmail.com>
2016-08-10 18:19:17 -0400

    remove solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    # Now that our database is configured, our Tweets will persist on every refresh, even if we restart the server. 
-    @tweets = Tweet.all
     erb :index
   end
 
@@ -20,10 +18,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/tweet' do
-    # Tweet.new(params[:username], params[:status])
-    # When creating new tweets, ActiveRecord uses hash syntax - we pass in a hash where the key is the column name and the value is whatever we want. 
-    tweet = Tweet.new({:username => params[:username], :status => params[:status]}) # We save the tweet into an instance variable. 
-    tweet.save # We then save it to the database using the "save" method
     redirect '/'
   end
 
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,25 +1,25 @@
 # Our Tweet class now inherits from ActiveRecord::Base. ActiveRecord now gives us access to tons of methods, including username and status methods for instances of Tweets, and an  'all' method for the class of Tweet. 
 # Because we're inheriting all of this from ActiveRecord, we can comment out all of the code that we wrote on days 1 and 2.
-class Tweet < ActiveRecord::Base
-  # attr_reader :username, :status
+class Tweet
+  attr_reader :username, :status
   
-  # ALL = []
+  ALL = []
 
-  # def initialize(username, status)
-  #   @username = username
-  #   @status = status
-  #   ALL << self
-  # end
+  def initialize(username, status)
+    @username = username
+    @status = status
+    ALL << self
+  end
 
-  # def username
-  #   @username
-  # end
+  def username
+    @username
+  end
 
-  # def status
-  #   @status
-  # end
+  def status
+    @status
+  end
 
-  # def self.all
-  #   ALL
-  # end
+  def self.all
+    ALL
+  end
 end
